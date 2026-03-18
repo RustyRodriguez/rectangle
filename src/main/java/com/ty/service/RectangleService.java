86c8a59ce824f8864dd2d6d066b315a72cf6f4a1
Ty Rodriguez <ty.rodriguez2021@gmail.com>
2026-03-17 22:09:10 -0500

    feat: add rectangle service and method to check containment

---
+++ src/main/java/com/ty/service/RectangleService.java
@@ -0,0 +1,21 @@
+package com.ty.service;
+
+import com.ty.model.Rectangle;
+
+public class RectangleService {
+
+    /**
+     * Determines whether the outer rectangle contains the inner rectangle
+     *
+     * @param outer the rectangle that may contain the other
+     * @param inner the rectangle being tested for containment
+     * @return true if the inner rectangle is contained in the outer rectangle else false
+     */
+    public boolean contains(Rectangle outer, Rectangle inner) {
+        return outer.getLeft() <= inner.getLeft() &&
+                outer.getBottom() <= inner.getBottom() &&
+                outer.getRight() >= inner.getRight() &&
+                outer.getTop() >= inner.getTop();
+    }
+
+}
